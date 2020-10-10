3a3cdaff0b4c0080fcb86a96ff87c21c9656a706
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-10 12:52:49 +0800

    Sending basic info request

--- components/FridayDeviceList.tsx
+++ components/FridayDeviceList.tsx
@@ -1,6 +1,16 @@
-import {parseManufacturerData, stringToByteArray} from '@fridayhome/messages';
+import {
+  BasicInfoRequest,
+  Characteristics,
+  DeviceType,
+  Envelope,
+  KnownKeyIDs,
+  parseManufacturerData,
+  ProtocolV1,
+  Services,
+  stringToBytes,
+} from '@fridayhome/messages';
 import {decode} from 'base-64';
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {BleManager, State} from 'react-native-ble-plx';
 import {FridayDevice} from '../src/FridayDevice';
@@ -22,17 +32,16 @@ export const FridayDeviceList = () => {
             return;
           }
 
-          const bytes = base64ToBytes(device.manufacturerData);
-          const manufacturerData = parseManufacturerData(bytes);
+          const manufacturerData = parseManufacturerData(
+            base64ToBytes(device.manufacturerData),
+          );
 
           if (!manufacturerData?.isFriday) {
             return;
           }
-          console.debug(bytes);
-          console.log(manufacturerData.manufacturerId);
+          console.log(`Discovered ${manufacturerData.manufacturerId}`);
 
-          const fridayDevice = device as FridayDevice;
-          fridayDevice.friday = manufacturerData;
+          const fridayDevice = new FridayDevice(device, manufacturerData);
 
           setDevices((ds) =>
             ds
@@ -50,12 +59,29 @@ export const FridayDeviceList = () => {
     });
   }, [manager, setDevices]);
 
+  const send = useCallback(
+    (fridayDevice: FridayDevice) => () => {
+      fridayDevice.device.connect().then(() => {
+        console.log(`Connected to ${fridayDevice.friday.manufacturerId}`);
+        fridayDevice.device.discoverAllServicesAndCharacteristics().then(() => {
+          fridayDevice.monitorUno();
+          const message = new BasicInfoRequest(
+            new ProtocolV1(1, new Date(Date.now())),
+          );
+          const envelope = new Envelope(KnownKeyIDs.NoKeyID, message);
+          fridayDevice.sendUno(envelope.toBytes());
+        });
+      });
+    },
+    [],
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>List of Friday devices</Text>
-      {devices.map((x, i) => (
-        <Text key={i} style={styles.device}>
-          {x.name}
+      {devices.map((device, i) => (
+        <Text key={i} style={styles.device} onPress={send(device)}>
+          {DeviceType[device.friday.type]} - {device.friday.manufacturerId}
         </Text>
       ))}
     </View>
@@ -78,5 +104,5 @@ function base64ToBytes(str?: string | null): number[] {
   if (!str) {
     return [];
   }
-  return stringToByteArray(decode(str));
+  return stringToBytes(decode(str));
 }
--- src/FridayDevice.ts
+++ src/FridayDevice.ts
@@ -1,6 +1,58 @@
-import {Advertisement} from '@fridayhome/messages';
+import {Advertisement, Characteristics, Services} from '@fridayhome/messages';
+import {encode} from 'base-64';
 import {Device} from 'react-native-ble-plx';
 
-export interface FridayDevice extends Device {
+export class FridayDevice {
+  device: Device;
   friday: Advertisement;
+
+  constructor(device: Device, advertisement: Advertisement) {
+    this.device = device;
+    this.friday = advertisement;
+  }
+
+  public sendUno(bytes: number[]) {
+    console.debug(`Sending to ${this.friday.manufacturerId}: ${bytes}`);
+    const str = bytes.map((x) => String.fromCharCode(x)).join('');
+    const value = encode(str);
+
+    this.device
+      .writeCharacteristicWithoutResponseForService(
+        Services.UnoPrimary,
+        Characteristics.UnoRx,
+        value,
+      )
+      .then((_) => console.debug('data sent'))
+      .catch((err) => console.warn(err));
+  }
+
+  public monitorUno() {
+    this.device.monitorCharacteristicForService(
+      Services.UnoPrimary,
+      Characteristics.UnoTx,
+      (err, char) => {
+        if (err) {
+          console.warn(err);
+          return;
+        }
+
+        console.log(char?.value);
+      },
+    );
+  }
+
+  public printAllServicesAndCharacteristics() {
+    this.device.services().then((ss) =>
+      ss.forEach((s) => {
+        s.characteristics().then((cs) => {
+          console.debug(`Service: ${s.uuid}`);
+          cs.forEach((c) => {
+            console.debug(
+              `Characteristic ${c.uuid} IsNotifiable: ${c.isNotifiable}`,
+            );
+          });
+        });
+      }),
+    );
+  }
 }
